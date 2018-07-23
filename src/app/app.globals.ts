8ec872e3ae3de6cf643f2aee38b89aaccc74b57f
mathc <comandin1u@etu.univ-lorraine.fr>
2018-07-23 20:51:22 +0200

    header

--- src/app/app.globals.ts
+++ src/app/app.globals.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppGlobals {
 
-    //readonly apiURL: string = 'http://localhost:82/';
-    readonly apiURL: string = 'http://webservice.jcs-infinity.fr:82/';
+    readonly apiURL: string = 'http://localhost:82/';
+    //readonly apiURL: string = 'http://webservice.jcs-infinity.fr:82/';
 }

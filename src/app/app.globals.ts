3ba9f55e0a80c189c3e84c33852149e1fab89190
Forandos <math.comandini@outlook.fr>
2020-07-20 22:38:46 +0200

    test conflit

--- src/app/app.globals.ts
+++ src/app/app.globals.ts
@@ -6,4 +6,6 @@ export class AppGlobals {
     readonly apiURL: string = 'http://localhost:82/';
     //readonly apiURL: string = 'http://webservice.jcs-infinity.fr:82/';
     //readonly apiURL: string = 'https://webservice.jcs-infinity.fr:82/';
+
+    readonly helloworld: string = "coucou";
 }
